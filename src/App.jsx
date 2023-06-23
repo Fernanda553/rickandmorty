ac03159b0448f0de856440495859fcf8459bc192
fernanda nuñez <fernanda.nunez55@gmail.com>
2023-06-23 17:43:58 -0400

    change names of components

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,12 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-import { useState, useEffect } from "react";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import Selector from "./assets/components/Selector.jsx";
-import Cards from "./assets/components/Cards.jsx";
+import MiApi from "./assets/components/MiApi.jsx";
 
 function App() {
-  const [characters, setCharacters] = useState([]);
-
-  async function getData(url) {
-    const response = await fetch(url);
-    const info = await response.json();
-    return info;
-  }
-
-  const getCharacter = async () => {
-    try {
-      const character = await getData(
-        "https://rickandmortyapi.com/api/character"
-      );
-      setCharacters(character.results);
-    } catch (error) {
-      console.error(error.message);
-    }
-  };
-
-  useEffect(() => {
-    getCharacter();
-  }, []);
-
   return (
-    <Container fluid>
-      <h1 className="p-4">Find your character</h1>
-      <Container fluid>
-        <Row>
-          <Col className="m-4 p-4">
-            <Selector />
-          </Col>
-        </Row>
-        <Row>
-          <Cards characters={characters} />
-        </Row>
-      </Container>
-    </Container>
+    <>
+      <MiApi />
+    </>
   );
 }
 
---
+++ src/assets/components/MiApi.jsx
@@ -0,0 +1,74 @@
+import { useState, useEffect } from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Buscador from "./Buscador";
+import Cards from "./Cards";
+
+const MiApi = () => {
+  const [characters, setCharacters] = useState([]);
+  const [selector, setSelector] = useState("");
+
+  const getData = async (url) => {
+    const response = await fetch(url);
+    const info = await response.json();
+    return info;
+  };
+
+  const getCharacter = async () => {
+    try {
+      const character = await getData(
+        "https://rickandmortyapi.com/api/character"
+      );
+      setCharacters(character.results);
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+
+  const handlerChange = (e) => {
+    setSelector(e.target.value);
+  };
+
+  const sendCharacter = async (e) => {
+    e.preventDefault();
+
+    const characterName = await getData(
+      "https://rickandmortyapi.com/api/character"
+    );
+    const dataOfNames = characterName.results;
+    const names = dataOfNames.map((n) => n.name);
+    const sortedNames = names.sort();
+    console.log(sortedNames);
+
+    /*  const found = names.some((obj) => {
+      return obj === selector;
+    }); */
+  };
+
+  useEffect(() => {
+    getCharacter();
+  }, []);
+
+  return (
+    <Container fluid>
+      <h1 className="p-4">Find your character</h1>
+      <Container fluid>
+        <Row>
+          <Col className="m-4 p-4">
+            <Buscador
+              selector={selector}
+              sendCharacter={sendCharacter}
+              handlerChange={handlerChange}
+            />
+          </Col>
+        </Row>
+        <Row>
+          <Cards characters={characters} />
+        </Row>
+      </Container>
+    </Container>
+  );
+};
+
+export default MiApi;
